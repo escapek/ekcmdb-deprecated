0cc32ca20bfc108725933e984f663ee48af2df4c
Nicolas Jouanin <nicolas.jouanin@gmail.com>
2011-02-28 17:05:42 +0100

    Implement contents() to get schema content

--- model-neo4j/src/main/scala/org/escapek/ekcmdb/model/neo4j/SchemaImpl.scala
+++ model-neo4j/src/main/scala/org/escapek/ekcmdb/model/neo4j/SchemaImpl.scala
@@ -2,13 +2,13 @@ package org.escapek.ekcmdb.model.neo4j
 
 import org.escapek.ekcmdb.model.CIClass
 import org.escapek.ekcmdb.model.Schema
-import org.neo4j.graphdb.Node
+import org.neo4j.graphdb.{Direction, Node}
 
 class SchemaImpl(override val node: Node) extends ModelElementImpl(node) with Schema
 {
-/*	override def content : Set[CIClass] = {
-		node.getRelationships
+	override def content : Set[CIClass] = {
+    val iterator =
+      node.getRelationships(RepositoryRelationships.Rel_ClassBelongsToSchema,Direction.INCOMING).iterator
+    Set.empty[CIClass] ++ iterator.map( c => new MetaDataImpl(c.getEndNode()))
 	}
-*/
-  def content = null
 }
