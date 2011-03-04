b49e123903ae3f6b268eed60da67e87c405dd2d2
Nicolas Jouanin <nicolas.jouanin@gmail.com>
2011-03-04 16:00:30 +0100

    Add dao module

---
+++ dao/src/main/scala/org/escapek/ekcmdb/dao/CIClassDao.scala
@@ -0,0 +1,15 @@
+package org.escapek.ekcmdb.dao
+
+import org.escapek.ekcmdb.model.CIClass
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Jouanin
+ * Date: 04/03/11
+ * Time: 15:48
+ */
+
+trait CIClassDao {
+  abstract def getByName(className : String) : CIClass
+
+}
