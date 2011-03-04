e49acca148ec5cd4674b7e2555fe8c4f6c4a2f74
Nicolas Jouanin <nicolas.jouanin@gmail.com>
2011-03-04 09:03:09 +0100

    Cleanup

---
+++ model-neo4j/src/test/scala/org/escapek/ekcmdb/model/neo4j/RepositoryElementSpec.java
@@ -0,0 +1,11 @@
+package org.escapek.ekcmdb.model.neo4j;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Jouanin
+ * Date: 01/03/11
+ * Time: 17:03
+ * To change this template use File | Settings | File Templates.
+ */
+public class RepositoryElementSpec {
+}
