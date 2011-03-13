7ee4203d8af2877229f93506517c586cde0dbdcc
Nicolas Jouanin <nicolas.jouanin@gmail.com>
2011-03-13 21:59:15 +0100

    Make OSGi bundle

---
+++ services-sample/src/main/scala/org/escapek/ekcmdb/services/SchemasResource.scala
@@ -0,0 +1,15 @@
+package org.escapek.ekcmdb.services
+
+import javax.ws.rs.Path
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: nico
+ * Date: 09/03/11
+ * Time: 22:13
+ */
+
+@Path("/schema")
+class SchemasResource {
+  
+}
